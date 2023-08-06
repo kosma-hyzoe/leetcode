64fee3abd9cc44245e3d54ca33c5c8c0a8123951
Kosma <kosma@pop-os.localdomain>
2023-08-06 13:10:05 +0200

    automated quick commit

---
+++ graphs/clone_graph.py
@@ -0,0 +1,41 @@
+'''
+https://leetcode.com/problems/clone-graph/
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its
+neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+
+from copy imporgct deepcopy
+'''
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+class Solution:
+	def cloneGraph(self, node: 'Node') -> 'Node':
+		if not node:
+			return None
+		copy = Node(node.val, [])
+
+		q = [node]
+		copy_dict = {node: Node(node.val)}
+
+		while q:
+			curr = q.pop(0)
+			for nbr in curr.neighbors:
+				if nbr not in copy_dict:
+					copy_dict[nbr] = Node(nbr.val)
+					q.append(nbr)
+				copy_dict[curr].neighbors.append(copy_dict[nbr])
+
+		return copy_dict[node]
---
+++ graphs/course_schedule.py
@@ -0,0 +1,48 @@
+'''
+https://leetcode.com/problems/course-schedule
+
+There are a total of numCourses courses you have to take, labeled from 0 to
+numCourses - 1. You are given an array prerequisites where prerequisites[i] =
+[ai, bi] indicates that you must take course bi first if you want to take course
+ai.
+
+    For example, the pair [0, 1], indicates that to take course 0 you have to
+    first take course 1.
+
+Return true if you can finish all courses. Otherwise, return false.
+'''
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites):
+        if not prerequisites:
+            return True
+
+        prerequisites_count = [0] * numCourses
+        prerequisites_list = {}
+
+        for a, b in prerequisites:
+            prerequisites_count[a] += 1
+            if b not in prerequisites_list:
+                prerequisites_list[b] = []
+            prerequisites_list[b].append(a)
+
+        q = []
+        for c in range(numCourses):
+            if prerequisites_count[c] == 0:
+                q.append(c)
+
+        courses_taken = 0
+        while q:
+            curr = q.pop(0)
+            courses_taken += 1
+
+            if not prerequisites_list.get(curr):
+                continue
+            for c in prerequisites_list[curr]:
+                prerequisites_count[c] -= 1
+
+                if prerequisites_count[c] == 0:
+                    q.append(c)
+        return courses_taken == numCourses
+
