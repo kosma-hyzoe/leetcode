b0892b4cbd7139fc0f7aa879b3b2a4f50b5540de
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2023-09-12 23:25:09 +0200

    automated quick commit

--- arrays/valid_sudoku.py
+++ arrays/valid_sudoku.py
@@ -3,24 +3,7 @@ from collections import namedtuple
 P = namedtuple("Point", ['x', 'y'])
 
 BOARD_SIZE = 9
-LEGAL_NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-BOX_POINTS = [P(1, 1), P(4, 1), P(7, 1),
-              P(1, 4), P(4, 4), P(7, 4),
-              P(1, 7), P(4, 7), P(7, 7)]
-# boxes = {point: set() for point in BOXES_POINTS}
-
-
-def is_box_valid(p, board):
-    box = set()
-    for y in range(p.y - 1, p.y + 2):
-        for x in range(p.x - 1, p.x + 2):
-            val = board[y][x]
-            if val == ".":
-                continue
-            if val in box:
-                return False
-            box.add(val)
-    return True
+_boxes =  [[set() for _ in range(3)] for _ in range(3)]
 
 
 class Solution:
@@ -33,15 +16,25 @@ class Solution:
                     continue
 
                 num = board[y][x]
-                if num in columns[x] or num in rows[y]:
+                box = _boxes[y // 3][x // 3]
+                if num in box or  num in columns[x] or num in rows[y]:
                     return False
+                box.add(num)
                 columns[x].add(num)
                 rows[y].add(num)
-
-        for p in BOX_POINTS:
-            if not is_box_valid(p, board):
-                return False
         return True
 
-board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
-Solution().isValidSudoku(board)
+
+board = [
+    [".","8","7","6","5","4","3","2","1"],
+    ["2",".",".",".",".",".",".",".","."],
+    ["3",".",".",".",".",".",".",".","."],
+    ["4",".",".",".",".",".",".",".","."],
+    ["5",".",".",".",".",".",".",".","."],
+    ["6",".",".",".",".",".",".",".","."],
+    ["7",".",".",".",".",".",".",".","."],
+    ["8",".",".",".",".",".",".",".","."],
+    ["9",".",".",".",".",".",".",".","."]
+]
+
+assert Solution().isValidSudoku(board)
