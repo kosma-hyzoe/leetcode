bc3e3115256e14f0777df50b57b2fd6350c4485c
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2023-09-12 23:44:13 +0200

    automated quick commit

--- arrays/valid_sudoku.py
+++ arrays/valid_sudoku.py
@@ -1,13 +1,12 @@
 from collections import namedtuple
 
 P = namedtuple("Point", ['x', 'y'])
-
 BOARD_SIZE = 9
-_boxes =  [[set() for _ in range(3)] for _ in range(3)]
 
 
 class Solution:
     def isValidSudoku(self, board):
+        boxes = [[set() for _ in range(3)] for _ in range(3)]
         columns = [set() for _ in range(BOARD_SIZE)]
         rows = [set() for _ in range(BOARD_SIZE)]
         for y in range(BOARD_SIZE):
@@ -16,8 +15,8 @@ class Solution:
                     continue
 
                 num = board[y][x]
-                box = _boxes[y // 3][x // 3]
-                if num in box or  num in columns[x] or num in rows[y]:
+                box = boxes[y // 3][x // 3]
+                if num in box or num in columns[x] or num in rows[y]:
                     return False
                 box.add(num)
                 columns[x].add(num)
