4381c57b05b35d1385986be7f39255ecb0178c9c
Marcin Kosma Hyżorek <kosma.hyzoe@gmail.com>
2025-02-08 19:46:23 +0100

    automated quick commit

--- two-pointers/threesum_take2.py
+++ two-pointers/threesum_take2.py
@@ -16,35 +16,25 @@ class Solution:
         # if nums[0] == 0:
         #     return [[0, 0, 0]] if nums[:3] == [0, 0, 0] else []
 
-        prev = 'f'
         for i in range(len(nums) - 2):
-
-            if nums[i] == prev:
-                prev = nums[i]
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            else:
-                prev = nums[i]
 
             right = len(nums) - 1
-            prev_left = 'f'
             left = i + 1
-            while left < len(nums) - 1 and right != left:
-                if prev_left == nums[left]:
-                    right = len(nums) - 1
-                    prev_left = nums[left]
-                    left += 1
-                    continue
+            while left < right:
                 sum = nums[i] + nums[left] + nums[right]
                 if sum == 0:
                     triplets.append([nums[i], nums[left], nums[right]])
-                    right -= 1
-                    if right == left:
-                        break
-                    prev_left = nums[left]
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
                     left += 1
+                    right -= 1
 
                 elif sum < 0:
-                    prev_left = nums[left]
                     left += 1
                 elif sum > 0:
                     right -= 1
